7b8b6280ec0fa9a7be698cc2cf6e6ecf37d3e969
euxzy <rijal.m12l@gmail.com>
2022-11-12 14:55:15 +0700

    setup prettier

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+// import { Link, Head } from '@inertiajs/inertia-react'
+
+const Home = () => {
+    return (
+        <div className="flex h-screen w-full items-center justify-center">
+            <h1 className="text-5xl">Hello World!</h1>
+        </div>
+    )
+}
+
+export default Home
--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,22 +1,21 @@
-import './bootstrap';
-import '../css/app.css';
+import '../css/app.css'
 
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { createInertiaApp } from '@inertiajs/inertia-react';
-import { InertiaProgress } from '@inertiajs/progress';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { createInertiaApp } from '@inertiajs/inertia-react'
+import { InertiaProgress } from '@inertiajs/progress'
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
 
-const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
+const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Kodasyok'
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
     setup({ el, App, props }) {
-        const root = createRoot(el);
+        const root = createRoot(el)
 
-        root.render(<App {...props} />);
-    },
-});
+        root.render(<App {...props} />)
+    }
+})
 
-InertiaProgress.init({ color: '#4B5563' });
+InertiaProgress.init({ color: '#4B5563', showSpinner: true })
